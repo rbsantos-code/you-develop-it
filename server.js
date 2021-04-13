fd63f6dcea8e1ad8833e834e2e46f014b6a6a7e0
rbsantos-code <richardbriansantos@richards-mbp.attlocal.net>
2021-04-12 20:11:17 -0700

    database calls

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,6 +14,24 @@ app.use(express.json());
 // route to handle user request that are not supported
 app.use((req, res) => {
     res.status(404).end();
+});
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: 'root',
+        // your MySQL password,
+        password: 'RBBootcamp91',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
+// return all the data in the candidates tables -12.2.4
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // function to start EXPRESS
